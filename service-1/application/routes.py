8a4de84314171c511f8ca6d6e7ba283b39231b30
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-12-02 11:11:16 +0000

    full service

---
+++ service-1/application/routes.py
@@ -0,0 +1,14 @@
+@app.route('/')
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html', title='click the lotto button')
+
+
+@app.route('/lotto', methods=['GET', 'POST'])
+def lotto():
+    alpha=requests.get('http://service-2:5001/alpha')
+    number=requests.get('http://service-3:5002/number')
+    ticket= str(alpha.text) + str(number.text)
+    prize=requests.post('http://service-4:5003/prize', data=ticket)
+    return render_template('lotto.html', title='lotto draw is', ticket=ticket, prize=prize.text)

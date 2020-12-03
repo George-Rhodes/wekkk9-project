90b7ceea88992c4ca0242f0a0652b2807b2fbf6f
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-12-03 15:22:59 +0000

    database

--- service-1/application/models.py
+++ service-1/application/models.py
@@ -4,6 +4,6 @@ from application import db
 
 class ticket_prize(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ticket = db.Column(db.String(6), nullable=False)
-    prize = db.Column(db.String(200), nullable=False)
+    ticket_num = db.Column(db.String(6), nullable=False)
+    prize_info = db.Column(db.String(200), nullable=False)
     

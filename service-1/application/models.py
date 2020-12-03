d297e2d50ce19e3e20e747f3beda4fd971afa52f
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-12-03 15:25:46 +0000

    database

--- service-1/application/models.py
+++ service-1/application/models.py
@@ -2,7 +2,7 @@ from application import db
 
 
 
-class ticket_prize(db.Model):
+class ticks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ticket_num = db.Column(db.String(6), nullable=False)
     prize_info = db.Column(db.String(200), nullable=False)
--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -1,6 +1,6 @@
 from flask import redirect, url_for, Response, request, render_template
 from application import app, db
-from application.models import ticket_prize
+from application.models import ticks
 import requests
 import random
 
@@ -17,7 +17,7 @@ def lotto():
     ticket= str(alpha.text) + str(number.text)
     prize=requests.post('http://service-4:5003/prize', data=ticket)
     prize_str = str(prize.text)
-    ted= ticket_prize(ticket_num=ticket, prize_info=prize_str)
+    ted= ticks(ticket_num=ticket, prize_info=prize_str)
     db.session.add(ted)
     db.session.commit()
     return render_template('lotto.html', title='lotto draw is', ticket=ticket, prize=prize.text)
